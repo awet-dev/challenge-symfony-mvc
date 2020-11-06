2b8f8db21f8547c93836584d917beb07a78a8eee
awet tesfay <whitezscope@gmail.com>
2020-11-06 16:05:51 +0100

    submit the form

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -5,40 +5,53 @@ namespace App\Controller;
 use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
-    private string $name = 'Unknown';
-    private Session $session;
+    private string $name;
+    private SessionInterface $session;
 
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
 
-    /**
-     * @Route("/learning", name="learning")
-     */
     public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
-
+            'controller_name' => 'LearningController'
         ]);
     }
 
-    public function aboutMe(): Response {
-        return $this->render('learning/aboutMe.twig');
+    public function aboutMe(): Response
+    {
+        return $this->render('learning/aboutMe.twig',[
+            'name' => 'BeCode'
+        ]);
     }
 
-
     public function showName(): Response {
-        return $this->render( 'learning/greetingform.twig',[
+        return $this->render('learning/showMyName.twig', [
             'name' => $this->name
         ]);
     }
 
-    public function changMyName(): Response {
-        return $this->render( 'learning/greetingForm.twig', [
-            $this->session->start(),
-            'name' => $this->session
+    public function changeMyName(): Response {
+        if (!empty($_POST['name'])) {
+            $this->name = $_POST['name'];
+        }  else {
+            $this->name = 'unknown';
+        }
+        return $this->render('learning/changeMyName.twig', [
+            'name' => $this->name
         ]);
     }
+
+
+
+
+
 }
