3ee76581d78bb0f726f6c080736296cbe5039c1c
awet tesfay <whitezscope@gmail.com>
2020-11-06 15:04:53 +0100

    creating the form

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -2,25 +2,43 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
+    private string $name = 'Unknown';
+    private Session $session;
+
+
     /**
      * @Route("/learning", name="learning")
      */
-    public function aboutMe(): Response
+    public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
-            'controller_name' => 'LearningController',
-            'course_name' => 'web development',
+
         ]);
     }
 
-    public function test(): Response {
-        return $this->render('learning/example.twig', ['name' => 'BeCode']);
+    public function aboutMe(): Response {
+        return $this->render('learning/aboutMe.twig');
     }
 
+
+    public function showName(): Response {
+        return $this->render( 'learning/greetingform.twig',[
+            'name' => $this->name
+        ]);
+    }
+
+    public function changMyName(): Response {
+        return $this->render( 'learning/greetingForm.twig', [
+            $this->session->start(),
+            'name' => $this->session
+        ]);
+    }
 }
