9da843e6c69c169997d5e4303b5b8e38051f7549
Awet Tesfay <whitezscope@gmail.com>
2020-12-11 21:39:06 +0100

    refactors and understand wel

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -2,16 +2,14 @@
 
 namespace App\Controller;
 
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
-    private string $name = 'unknown';
+    private string $name = 'Unknown';
     private SessionInterface $session;
 
 
@@ -20,6 +18,9 @@ class LearningController extends AbstractController
         $this->session = $session;
     }
 
+    /**
+     * @Route("/learning", name="learning")
+     */
     public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
@@ -27,33 +28,46 @@ class LearningController extends AbstractController
         ]);
     }
 
+    public function setName() :string {
+        if ($this->session->get('name') !== null){
+            $this->name = $this->session->get('name');
+            return $this->name;
+        }
+        return $this->name;
+    }
+
+    /**
+     * @Route("/about-becode", name="aboutMe")
+     */
     public function aboutMe(): Response
     {
+        if ($this->setName() == "Unknown") {
+            return $this->forward('\App\Controller\LearningController::showMyName');
+        }
         return $this->render('learning/aboutMe.twig',[
-            'name' => 'BeCode'
+            'name' => $this->setName()
         ]);
     }
 
-    public function showName(): Response {
+    /**
+     * @Route("/", name="showMyName")
+     */
+    public function showMyName(): Response {
+
         return $this->render('learning/showMyName.twig', [
-            'name' => $this->name
+            'name' => $this->setName()
         ]);
     }
 
+    /**
+     * @Route ("/changeMyName", name="changeMyName", methods = {"POST"})
+     */
     public function changeMyName(): Response {
         if (!empty($_POST['name'])) {
-            $this->name = $_POST['name'];
-
-            return $this->render('learning/changeMyName.twig', [
-                'name' => $this->name
-            ]);
-        } else {
-            return $this->render('learning/showMyName.twig', [
-                'name' => "enter your name"
-            ]);
+            $this->session->set('name', ucwords($_POST['name']));
         }
 
-
+        return $this->redirectToRoute('showMyName');
     }
 
 
